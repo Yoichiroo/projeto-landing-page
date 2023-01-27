72d25c6b10072a7cbb4e82bfa1be1bd4ee75e446
Lucas Torres <repositoriolucas@gmail.com>
2023-01-27 17:36:06 -0300

    Estilos de header e main.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,1 @@
 
-function clickMenu() {
-    if (menuBar.style.display == 'block') {
-        menuBar.style.display = 'none'
-    } else {
-        menuBar.style.display == 'block'
-    }
-}
-
-function mudouTamanho() {
-    if(window.innerWidth > 768) {
-        menuBar.style.display = 'block'
-    } else {
-        menuBar.style.display = 'none'
-    }
-}
