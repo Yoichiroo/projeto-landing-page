13d55c7829b728e80d8b67308a3a79b582bb9717
Lucas Torres <repositoriolucas@gmail.com>
2023-01-27 18:09:14 -0300

    Versão de celular pronta.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,23 @@
+let menu = document.getElementById("menuBar")
+let telaPreta = document.getElementById("tela-preta")
+
+function clickMenu(){
+    if(menu.style.display == 'none' ) {
+        menu.style.display = 'block'
+        telaPreta.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+        telaPreta.style.display = 'none'
+    }
+}
+
+function fecharMenu() {
+    let menu = document.getElementById("menuBar")
+    if(menu.style.display == 'none' ) {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+        telaPreta.style.display = 'none'
+    }
+}
 
